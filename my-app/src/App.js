c40a83a1ca0bb16e0fdd2e1e1cf3ae9e0e4597ff
AlexAntony3 <alex.antony.jy@gmail.com>
2023-09-21 10:58:17 +0000

    feat: second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import css from './App.module.css';
-import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className={css.App}>
       {<Sidebar />}
+
+      <StatefulGreetingWithPrevState greeting="I'm a stateful class component" name="Alex"/>
     </div>
   );
 }
